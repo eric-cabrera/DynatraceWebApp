f263709bd370de4c5eb0008ed10573526f9c2992
Eric Cabrera <ecabrera@assurity.com>
2025-05-23 20:53:27 -0400

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DynatraceWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
